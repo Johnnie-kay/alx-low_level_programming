34cba8fe134cf516aa0f25413fe899941e97e5e9
Isujah Johnson <johnniekay85@gmail.com>
2023-02-19 17:41:03 -0800

    correction

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,18 +2,22 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char low;
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
-
-		if (low == 'q' || low == 'e')
-		continue;
+	{
+		if (low != e && low != q)
 			putchar(low);
+	}
 	putchar('\n');
+
 	return (0);
 }
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,6 @@
-#include <stdio>
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
 /**
  * main - Entry point
  *
@@ -10,7 +12,6 @@ int main(void)
 	char i;
 
 	for (i = 'z'; i >= 'a'; i--)
-
 		putchar(i);
 	putchar('\n');
 	return (0);
