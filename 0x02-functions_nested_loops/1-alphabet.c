379e46dbd0602c5446f43cb7f8b559c38cf21b65
Isujah Johnson <johnniekay85@gmail.com>
2023-02-28 13:29:27 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ *
+ * Return:0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while(c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	putchar("\n");
+	
+}
