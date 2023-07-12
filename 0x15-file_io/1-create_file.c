b523796274cc1f59ea32f3c271ac26daee2a9210
Johnson Ikechukwu <johnniekay85@gmail.com>
2023-07-12 03:44:57 -0700

    task 2_correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,50 @@
-#include "main.h"
-#include <stddef.h>
-#include <unistd.h>
+#include "holberton.h"
 
 /**
- * create_file - function that creates a file
- * @filename: pointer that points to the text
- * @text_content: a pointer to str content
- * Return: success(0), failure(-1) 
+ * _strlen - computes the length of a NULL-terminated string
+ * @str: string to be measured
+ *
+ * Return: the length of string, or (-1) if string is NULL
  */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
 
+	if (!str)
+		return (-1);
+
+	while (*str++)
+		++len;
+
+	return (len);
+}
+
+/**
+ * create_file - create a file
+ * @filename: the name of the file to create
+ * @text_content: the data to write to filename
+ *
+ * Return: success (1), failure (-1)
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, length, i;
+	ssize_t b_written = 0;
+	int fd;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (fd == -1)
+	if (fd < 0)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (i = 0; text_content[i]; i++)
+	if (text_content)
+		b_written = write(fd, text_content, _strlen(text_content));
 
-			length = write(fd, text_content, i);
-		if (length == -1)
-			return (-1);
-	}
 	close(fd);
+
+	if (b_written < 0)
+		return (-1);
 	return (1);
 }
