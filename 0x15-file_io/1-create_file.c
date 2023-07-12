ea3c70b458a22171f4327cfa0249fa85c5ce75e3
Johnson Ikechukwu <johnniekay85@gmail.com>
2023-07-12 04:46:56 -0700

    task 2_correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,29 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
  * create_file - creates a file
- * @filename: the name of the file to create
- * @text_content: the data to write to filename
+ * @filename: pointer that points to name of the file to create
+ * @text_content: pointer to a string to write to the file
  *
- * Return: success (1), failure (-1)
+ * return: failure(-1)
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t b_written = 0;
-	int fd;
+	int fd, w, len = 0;
+	if (filename == NULL)
+		return(-1);
+	if (text_content != NULL)
+		for (len = 0; text_content[len];)
+			len++;
+	fd = open(filename, O_CREAT|O_RDWR|O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-	if (!filename)
-		return (-1);
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
-	if (fd < 0)
-		return (-1);
-
-	if (text_content)
-		b_written = write(fd, text_content, _strlen(text_content));
-
+	if (fd == -1 || w == -1)
+		return(-1);
 	close(fd);
-
-	if (b_written < 0)
-		return (-1);
 	return (1);
+
 }
