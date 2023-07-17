80b885234f6cffab6213854d6d259aa201ba08d3
Johnson Ikechukwu <johnniekay85@gmail.com>
2023-07-16 19:30:48 -0700

    trailing w_space

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - the node at a certain index in a linked list is returned
+ * get_nodeint_at_index - node at a certain index in a linked list is returned
  * @head: 0 node in the linked list
  * @index: the node returns to index
  * Return: points to the node we are finding, or NULL
